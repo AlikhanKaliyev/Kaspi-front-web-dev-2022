84d0136f3ac1f38b14a197c3edb08bcf83f67af2
Tair Sairanbekov <fsta@technodom.kz>
2022-12-27 18:03:45 +0600

    feat(shop): shop's main page

---
+++ src/pages/shop/constants.js
@@ -0,0 +1,55 @@
+export const links1 = [
+  {
+    link: '',
+    img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/4cd61e8e-7faa-4276-8aaf-44380e0c145d-TV.png',
+    title: 'ТВ, Аудио, Видео'
+  },
+  {
+    link: '',
+    img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/362c1530-38f2-4bfa-b65d-0d4e84f73f5e-Beauty.png',
+    title: 'Красота и здоровье'
+  },
+  {
+    link: '',
+    img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/2eac5475-390e-484c-8dc9-a3b3a6758abf-Avto.png',
+    title: 'Автотовары'
+  },
+  {
+    link: '',
+    img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/97a54c52-1572-4410-95e8-7027fc0464de-Kids.png',
+    title: 'Детские товары'
+  },
+  {
+    link: '',
+    img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/2e1540b8-0546-4741-acc4-447c0df3a981-Computer.png',
+    title: 'Компьютеры'
+  },
+]
+
+export const links2 = [
+  {
+    link: '',
+    img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/e2c63fc3-85c0-4985-adae-0b646ed91b2b-Hard.png',
+    title: 'Строительство и ремонт'
+  },
+  {
+    link: '',
+    img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/32ca8f3c-e27d-4bf4-a6cd-0f7ca16b9586-Clothes.png',
+    title: 'Одежда'
+  },
+  {
+    link: '',
+    img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/2a793ce4-bd93-40e8-be53-3b9c29fa1fc9-accessories-2.png',
+    title: 'Аксессуары'
+  },
+  {
+    link: '',
+    img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/d9057c79-2b13-4bf7-ab6a-3458d3cf31ea-Grocery.png',
+    title: 'Продукты питания'
+  },
+  {
+    link: '',
+    img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/c75cdd81-0da8-4fb0-ae47-de52fcbee721-Furniture.png',
+    title: 'Мебель'
+  },
+]
